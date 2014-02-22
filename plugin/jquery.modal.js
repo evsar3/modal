33d6c61ca2e2f44aa8e497a6f16ed44a4eb722a4
Evandro Araújo <evandroprogram@gmail.com>
2014-02-22 02:12:20 -0200

    License update

--- plugin/jquery.modal.js
+++ plugin/jquery.modal.js
@@ -1,11 +1,28 @@
 /*
-* License: https://www.gnu.org/copyleft/gpl.html
-* Author: Evandro Araújo
-* Location: Fortaleza, CE, Brazil
-* E-mail: evandroprogram@gmail.com
-* Public profile: https://fb.com/evandropontoaraujo
-* Date: Feb, 22, 2014
-* Version: 1.0.0
+ *    jQuery.modal - Modal over modal and persistant content plugin for jQuery
+ *   Copyright (C) 2014 Evandro Araújo
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Author: Evandro Araújo
+ * Location: Fortaleza, CE, Brazil
+ * E-mail: evandroprogram@gmail.com
+ * Public profile: https://fb.com/evandropontoaraujo
+ * Date: Feb, 22, 2014
+ * Version: 1.0.0
 */
 
 (function ($) {
