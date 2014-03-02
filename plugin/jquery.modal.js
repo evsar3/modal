61df153f292190131594f343534ee85c6d1da2c4
Evandro Araújo <evandroprogram@gmail.com>
2014-03-02 04:14:27 -0300

    Fade effect added on modal show. Some code improvements.

--- plugin/jquery.modal.js
+++ plugin/jquery.modal.js
@@ -47,97 +47,100 @@
           element.css('top', Math.max(0, (($(window).height() - element.outerHeight()) / 2)) + 'px');
           element.css('left', Math.max(0, (($(window).width() - element.outerWidth()) / 2)) + 'px');
         }
+      },
+
+      methods = {
+        show: function (options) {
+          var
+              _this = this,
+
+              defaults = {showOverlay: true,
+                          persistantContent: true,
+                          autoCenter: true,
+                          overlayClose: false,
+                          overlayOpacity: 0.5,
+                          overlayColor: '#000'},
+
+              options = $.extend(defaults, options);
+
+          // Stop function if modal window is already visible
+          if (_this.is(":visible")) return false;
+
+          // Set modal id
+          if (_this.attr(modal_attr) === undefined) {
+            var modal_id = Math.random().toString().replace('.', '');
+          } else {
+            var modal_id = _this.attr(modal_attr);
+          }
+
+          _this.attr(modal_attr, modal_id);
+
+          // Save content if persistantContent is true
+          if (!options.persistantContent) {
+            persistantContentFlagArray[modal_id] = false;
+            persistantContentArray[modal_id] = $('> *', _this).clone(true);
+          };
+
+          // Create modal overlay
+          if (options.showOverlay) {
+            var overlay = $('<div />').css({zIndex: (helpers.maxZIndex() + 1),
+                                            display: 'none',
+                                            position: 'fixed',
+                                            width: '100%',
+                                            height: '100%',
+                                            top: 0,
+                                            left: 0,
+                                            backgroundColor: options.overlayColor,
+                                            opacity: options.overlayOpacity})
+                                      .attr(modal_attr, modal_id)
+                                      .addClass('modal overlay')
+                                      .appendTo('body');
+          }
+
+          if (options.overlayClose) {
+            overlay.click(function () {
+              _this.modal('hide');
+            });
+          }
+
+          _this.css({zIndex: (helpers.maxZIndex() + 1),
+                     position: 'fixed'});
+
+          // Center modal window in screen
+          if (options.autoCenter) {
+            helpers.center(_this);
+
+            $(window).resize(function () {
+              helpers.center(_this);
+            });
+          }
+
+          // Show modal in the screen (effects can be used)
+          overlay.fadeIn(250);
+          _this.fadeIn(250);
+
+          // Move the focus to the first input element in modal window
+          $(':input:first', _this).focus();
+        },
+
+        hide: function () {
+          var
+              _this = this,
+              modal_id = _this.attr(modal_attr),
+              overlay = $('[' + modal_attr + '="' + modal_id + '"].modal.overlay');
+
+          _this.fadeOut(250, function () {
+            if (!persistantContentFlagArray[modal_id]) {
+              $('*', _this).remove();
+              _this.append(persistantContentArray[modal_id]);
+            }
+          });
+
+          overlay.fadeOut(250, function () {
+            overlay.remove();
+          });
+        }
       }
-
-  var methods = {
-    show: function (options) {
-      var
-          _this = this,
-         
-          defaults = {showOverlay: true,
-                      persistantContent: true,
-                      autoCenter: true,
-                      overlayClose: false,
-                      overlayOpacity: 0.5,
-                      overlayColor: '#000'},
-         
-          options = $.extend(defaults, options);
-
-      // Stop function if modal window is already visible
-      if (_this.is(":visible")) return false;
-
-      // Set modal id
-      if (_this.attr(modal_attr) === undefined) {
-        var modal_id = Math.random().toString().replace('.', '');
-      } else {
-        var modal_id = _this.attr(modal_attr);
-      }
-
-      _this.attr(modal_attr, modal_id);
-
-      // Save content if persistantContent is true
-      if (!options.persistantContent) {
-        persistantContentFlagArray[modal_id] = false;
-        persistantContentArray[modal_id] = $('> *', _this).clone(true);
-      };
-
-      // Create modal overlay
-      if (options.showOverlay) {
-        $('<div />').css({zIndex: (helpers.maxZIndex() + 1),
-                          display: 'none',
-                          position: 'fixed',
-                          width: '100%',
-                          height: '100%',
-                          top: 0,
-                          left: 0,
-                          backgroundColor: options.overlayColor,
-                          opacity: options.overlayOpacity})
-                    .attr(modal_attr, modal_id)
-                    .addClass('modal overlay')
-                    .appendTo('body');
-      }
-
-      if (options.overlayClose) {
-        $('[' + modal_attr + '="' + modal_id + '"].modal.overlay').click(function () {
-          _this.modal('hide');
-        });
-      };
-
-      _this.css({zIndex: (helpers.maxZIndex() + 1),
-                 position: 'fixed'});
-
-      // Center modal window in screen
-      if (options.autoCenter) {
-        helpers.center(_this);
-        
-        $(window).resize(function () {
-          helpers.center(_this);
-        });
-      }
-
-      // Show modal in the screen (effects can be used)
-      $('[' + modal_attr + '="' + modal_id + '"].modal.overlay').show();
-      _this.show();
-
-      // Move the focus to the first input element in modal window
-      $(':input:first', _this).focus();
-    },
-
-    hide: function () {
-      var 
-          _this = this,
-          modal_id = _this.attr(modal_attr);
-
-      _this.hide();
-
-      $('[' + modal_attr + '="' + modal_id + '"].modal.overlay').hide().remove();
-
-      if (!persistantContentFlagArray[modal_id]) {
-        $('*', _this).remove();
-        _this.append(persistantContentArray[modal_id]);
-      }
-    }
-  }
 
   jQuery.fn.modal = function (methodOrOptions) {
     if (methods[methodOrOptions]) {
